983cba1a1f949d2be674f6e788c40e11cf31a7c2
Asif Ansari <iamasif11996@gmail.com>
2026-09-21 20:28:00 +0530

    Update ApiData.jsx

--- src/api/ApiData.jsx
+++ src/api/ApiData.jsx
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://countries.dev",
-});
+const api =axios.create({
+baseURL:"https://restcountries.com/v5",
+})
 
-// get all
-export const getMethod = () => {
-  return api.get("/all?fields=name,capital,currencies,flags,region,population");
-};
+// get method
 
-// get individual
-export const getIndividualMethod = (name) => {
-  return api.get(`/name/${name}?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags`);
-};
+export const getMethod=()=>{
+return api.get("/all?fields=name,capital,currencies,flags,region,population")
+}
+
+// get indivdual method
+
+export const getIndividualMethod=(name)=>{
+return api.get(/name/${name}?fullText=true&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags)
+}
